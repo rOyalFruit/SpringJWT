a3d32a019b8356ebae2da09e527e6f947816912e
rOyalFruit <ekdeon@gmail.com>
2025-02-18 18:54:16 +0900

    feat: MainController에서 현재 사용자의 세션 정보 출력
    - SecurityContextHolder를 사용하여 현재 인증된 사용자의 세션 정보 조회
    - 사용자 name과 role을 컨트롤러 응답에 포함

--- backend/src/main/java/com/ll/backend/controller/MainController.java
+++ backend/src/main/java/com/ll/backend/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.ll.backend.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RestController
 public class MainController {
@@ -9,6 +15,16 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "main Controller";
+        // 세션에서 현재 사용자 name 가져오기
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // 세션에서 현재 사용자 role 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller: " + username + " " + role;
     }
 }
